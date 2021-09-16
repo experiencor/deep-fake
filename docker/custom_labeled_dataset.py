93e2888d3b6719fcb55fdd9557e39d2097622404
Andy <experiencor@gmail.com>
2021-09-16 22:22:15 +0800

    wip

---
+++ docker/custom_labeled_dataset.py
@@ -0,0 +1,64 @@
+import logging
+from typing import Any, Callable, Optional
+import cv2
+import numpy as np
+import torch
+import pickle
+import os
+
+import torch.utils.data
+
+from pytorchvideo.data.labeled_video_paths import LabeledVideoPaths
+
+
+logger = logging.getLogger(__name__)
+
+
+class CustomVideoDataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        data_path: str,
+        video_path_prefix: str = "",
+        transform: Optional[Callable[[dict], Any]] = None,
+        frame_number: int = 32,
+        frame_size: int = 224,
+        occluded_percent: float = 0.0,
+        inflation = 1,
+        augmentation = None,
+    ) -> None:
+        self._transform = transform
+        self._frame_number = frame_number
+        self._frame_size = frame_size
+        self._occluded_percent = occluded_percent
+        self._labeled_videos = LabeledVideoPaths.from_path(data_path)
+        self._labeled_videos.path_prefix = video_path_prefix
+        self._inflation = inflation
+        self._augmentation = augmentation
+        
+    def __len__(self):
+        return len(self._labeled_videos) * self._inflation
+    
+    def __getitem__(self, video_index):
+        video_index = video_index % len(self._labeled_videos)
+        video_path, info_dict = self._labeled_videos[video_index]
+
+        file_name = video_path.split("/")[-1]
+        frames = np.load(f"/data/face/{file_name}.npy")
+                
+        missing_frames = self._frame_number - len(frames)
+        frames = torch.tensor(np.stack(frames))
+        frames = torch.nn.functional.pad(
+            frames, 
+            (0, 0, 0, 0, 0, 0, 0, missing_frames), 
+        )
+        frames = frames.permute(3, 0, 1, 2)
+
+        sample_dict = {
+            "video":        frames,
+            "video_index":  video_index,
+            **info_dict,
+        }
+        if self._transform is not None:
+            sample_dict = self._transform(sample_dict)
+
+        return sample_dict
