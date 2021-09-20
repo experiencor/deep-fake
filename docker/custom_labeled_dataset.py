aefec9ec88a417e86f30c690734dbb3d3f0c2727
ngocdung <dunghuynh110496@gmail.com>
2021-09-20 17:48:46 +0800

    deterministic

--- docker/custom_labeled_dataset.py
+++ docker/custom_labeled_dataset.py
@@ -45,8 +45,8 @@ class CustomVideoDataset(torch.utils.data.Dataset):
     def __getitem__(self, video_index):
         if video_index == len(self._data_frame) - 1:
             log("=" * 25, self._epoch)
-            log(self, self._epoch, cls.abc)
-            abc += 1
+            log(self, self._epoch, CustomVideoDataset.abc)
+            CustomVideoDataset.abc += 1
             self._epoch += 10000
 
         data_row = self._data_frame.iloc[video_index]
