cd6aeedbdf3ba35a1e02b363ce2d6aef0f300c64
ngocdung <dunghuynh110496@gmail.com>
2021-09-20 16:51:44 +0800

    deterministic

--- docker/custom_labeled_dataset.py
+++ docker/custom_labeled_dataset.py
@@ -39,7 +39,7 @@ class CustomVideoDataset(torch.utils.data.Dataset):
     
     def __getitem__(self, video_index):
         data_row = self._data_frame.iloc[video_index]
-        log(data_row)
+        print(self._data_frame)
         filename = data_row["filename"] 
         label = int(data_row["label"])
 
