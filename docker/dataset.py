682908e284f996523ad42d1c4f3e5c5499b498e1
ngocdung <dunghuynh110496@gmail.com>
2021-09-21 19:35:27 +0800

    deterministic

--- docker/dataset.py
+++ docker/dataset.py
@@ -37,8 +37,7 @@ class Dataset(torch.utils.data.Dataset):
         for _, raw_frame in enumerate(raw_frames):
             frame = self._augmentation(image = raw_frame)["image"]
             frames += [frame]
-            print("sdfsdf")
-            cv2.imwrite(f"/data/temp/{filename}_{_}.png", frame)
+            #cv2.imwrite(f"/data/temp/{filename}_{_}.png", frame)
         missing_frames = self._frame_number - len(frames)
         frames = torch.tensor(np.stack(frames))
         frames = torch.nn.functional.pad(
