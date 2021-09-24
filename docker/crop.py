f27273294e00543ed5ee3153788c71365a724b83
ngocdung <dunghuynh110496@gmail.com>
2021-09-24 15:48:04 +0800

    wip

--- docker/crop.py
+++ docker/crop.py
@@ -1,8 +1,7 @@
-from facenet_pytorch import MTCNN
 import cv2
 import os
 import torch
-from facenet_pytorch import MTCNN
+from retinaface.pre_trained_models import get_model
 from multiprocessing import Process, JoinableQueue
 import traceback
 import json
@@ -18,16 +17,17 @@ fail_queue = JoinableQueue()
 
 def apply_crop(frame, boxes, probs, crop_threshold=0.975, margin=0.5):
     if boxes is None:
-        return frame, 0
+        return frame, -1
 
-    boxes = [box for box, prob in zip(boxes, probs) if prob > crop_threshold]
+    boxes = [box  for box, prob in zip(boxes, probs) if prob > crop_threshold]
     probs = [prob for prob in probs if prob > crop_threshold]
 
     if not boxes:
-        return frame, 0
+        return frame, -1
 
     face_idx = np.random.randint(len(boxes))
     x, y, z, t = boxes[face_idx]
+    x, y, z, t = int(x), int(y), int(z), int(t)
     width  = x - z
     height = t - y
     center_y = int((y + t) / 2)
@@ -42,12 +42,15 @@ def apply_crop(frame, boxes, probs, crop_threshold=0.975, margin=0.5):
     return frame[y:t, x:z, :], probs[face_idx]
 
 
-def crop_faces(mtcnn, frames, frame_size, crop_batch_size=4):
+def crop_faces(face_detector, frames, frame_size, crop_batch_size=8):
     all_boxes, all_probs = [], []
 
     for i in range(int(np.ceil(len(frames)/crop_batch_size))):
         batch = frames[i*crop_batch_size: (i+1)*crop_batch_size]
-        boxes, probs = mtcnn.detect(batch)
+        results = face_detector.predict_jsons_batch(batch)
+        boxes = [[box["bbox"]  for box in frame_result] for frame_result in results]
+        probs = [[box["score"] for box in frame_result] for frame_result in results]
+
         all_boxes += list(boxes)
         all_probs += list(probs)
 
@@ -63,12 +66,8 @@ def crop_faces(mtcnn, frames, frame_size, crop_batch_size=4):
 
 def worker(output_dir, save_image, no_cache, num_iters, device, frame_number, frame_size):
     try:
-        mtcnn = MTCNN(
-            margin = 14,
-            factor = 0.6,
-            keep_all = True,
-            device = device if torch.cuda.is_available() else "cpu"
-        )
+        face_detector = get_model("resnet50_2020-07-20", max_size=2048, device=device)
+        face_detector.eval()
     except Exception as e:
         log(e)
         return
@@ -92,30 +91,46 @@ def worker(output_dir, save_image, no_cache, num_iters, device, frame_number, fr
         try:
             if not os.path.exists(f"{output_dir}/0/{file_name}.npy") or no_cache:
                 video = cv2.VideoCapture(file_path)
-                
                 frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
                 total_count = num_iters * frame_number
-                if total_count <= frame_count:
-                    chosen_indices = np.random.choice(range(frame_count), total_count, replace=False)
+
+                all_the_indices = np.arange(frame_count)
+                np.random.shuffle(all_the_indices)
+                selected_indices = []
+                while len(all_the_indices) > 0:
+                    buffer_size     = int(1.2 * total_count)
+                    chosen_indices  = all_the_indices[:buffer_size]
+                    all_the_indices = all_the_indices[buffer_size:]
+                    chosen_indices  = sorted(chosen_indices)
+
+                    frames = []
+                    i, j, _ = 0, 0, video.grab()
+                    while len(frames) < len(chosen_indices):
+                        if i == chosen_indices[j]:
+                            _, frame = video.retrieve()
+                            frames += [frame]
+                            j += 1
+                        _ = video.grab()
+                        i += 1
+
+                    faces, probs = crop_faces(face_detector, frames, frame_size)
+                    for index, face, prob in zip(chosen_indices, faces, probs):
+                        if prob > 0:
+                            selected_indices += [[index, face, prob]]
+
+                    if len(selected_indices) >= total_count:
+                        break
+                    video = cv2.VideoCapture(file_path)
                 else:
-                    chosen_indices = np.range(frame_count)
-                chosen_indices = sorted(chosen_indices)
+                    log(f"found {len(selected_indices)} but required {total_count} for {file_name}")
 
-                frames = []
-                i, j, status = 0, 0, video.grab()
-                while len(frames) < total_count and status:
-                    if i == chosen_indices[j]:
-                        _, frame = video.retrieve()
-                        frames += [frame]
-                        j += 1
-                    status = video.grab()
-                    i += 1
+                selected_indices = sorted(selected_indices)
+                detect_faces = [face for _, face, _ in selected_indices[:total_count]]
+                detect_probs = [prob for _, _, prob in selected_indices[:total_count]]
 
-                faces, probs = crop_faces(mtcnn, frames, frame_size)
-
-                random_indices  = np.arange(len(faces))
+                random_indices  = np.arange(len(detect_faces))
                 l_idx, r_idx    = 0, frame_number
-                increment       = (len(faces) + 1 - frame_number) // num_iters
+                increment       = (len(detect_faces) + 1 - frame_number) // num_iters
                 np.random.shuffle(random_indices)
                 for iteration in range(num_iters):
                     iter_path = f"{output_dir}/{iteration}"
@@ -124,8 +139,8 @@ def worker(output_dir, save_image, no_cache, num_iters, device, frame_number, fr
                     select_indices = random_indices[l_idx:r_idx]
                     select_indices = sorted(select_indices)
 
-                    iter_faces = np.array([faces[i] for i in select_indices])
-                    iter_probs = np.array([probs[i] for i in select_indices])
+                    iter_faces = np.array([detect_faces[i] for i in select_indices])
+                    iter_probs = np.array([detect_probs[i] for i in select_indices])
                     np.save(f"{iter_path}/{file_name}.npy", iter_faces)
 
                     if save_image:
