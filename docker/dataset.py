ae49c86ff4c708a4ff03b00cc425301483ea94e8
ngocdung <dunghuynh110496@gmail.com>
2021-10-17 22:38:57 +0800

    audio_visual

--- docker/dataset.py
+++ docker/dataset.py
@@ -67,5 +67,5 @@ class Dataset(torch.utils.data.Dataset):
         if self._transform is not None:
             sample_dict = self._transform(sample_dict)
         
-        print("video", sample_dict["video"])
+        print("video", sample_dict["video"][0].shape, sample_dict["video"][1].shape)
         return sample_dict
