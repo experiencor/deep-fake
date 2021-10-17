aeac808bf232f8d45b45542e568c823cb2f20d9e
ngocdung <dunghuynh110496@gmail.com>
2021-10-17 22:48:33 +0800

    audio_visual

--- docker/dataset.py
+++ docker/dataset.py
@@ -57,6 +57,8 @@ class Dataset(torch.utils.data.Dataset):
         except Exception as e:
             log(e)
 
+        print("torch.tensor(mel)", torch.tensor(mel).shape)
+
         sample_dict = {
             "video": torch.permute(torch.tensor(faces), (3, 0, 1, 2)),
             "audio": torch.permute(torch.tensor(mel), (2, 0, 1)),
