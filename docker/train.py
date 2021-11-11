fa72bbe91705475698341eef7102cb56175f48aa
Andy <experiencor@gmail.com>
2021-11-11 12:24:35 +0800

    wip

--- docker/train.py
+++ docker/train.py
@@ -31,7 +31,6 @@ def main(args):
     run = wandb.init(project="deepfake", config=wandb_config)
     
     # set up the data
-    print("args.data_version", args.data_version)
     data_module = DataLoader(
         args.data_version,
         config["root_path"],
